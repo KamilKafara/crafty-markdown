868eca5038226bdd2ac48f70528fecbd6c0d33d9
Kamil Kafara <kafarakamil95@gmail.com>
2020-02-25 20:26:42 +0100

    initial commit

---
+++ src/main/java/pl/kafarsoon/craftymarkdown/CraftyMarkdownApplication.java
@@ -0,0 +1,13 @@
+package pl.kafarsoon.craftymarkdown;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CraftyMarkdownApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CraftyMarkdownApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/kafarsoon/craftymarkdown/CraftyMarkdownApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.kafarsoon.craftymarkdown;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CraftyMarkdownApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

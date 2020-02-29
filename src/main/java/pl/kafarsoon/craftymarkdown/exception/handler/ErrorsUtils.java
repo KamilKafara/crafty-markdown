7efcbf61b3536cfae3446dbbec507e0ccb75a0bc
Kamil Kafara <kafarakamil95@gmail.com>
2020-02-29 19:21:22 +0100

    CMD-5-exception-handling: add exception handlers

---
+++ src/main/java/pl/kafarsoon/craftymarkdown/exception/handler/ErrorsUtils.java
@@ -0,0 +1,25 @@
+package pl.kafarsoon.craftymarkdown.exception.handler;
+
+import lombok.NoArgsConstructor;
+import pl.kafarsoon.craftymarkdown.exception.BadRequestException;
+import pl.kafarsoon.craftymarkdown.exception.NotFoundException;
+
+@NoArgsConstructor
+public class ErrorsUtils {
+    public static void notFoundException(String message) {
+        throw new NotFoundException(message);
+    }
+
+    public static void notFoundException(String message, FieldInfo fieldInfo) {
+        throw new NotFoundException(message, fieldInfo);
+    }
+
+
+    public static void badRequestException(String message) {
+        throw new BadRequestException(message);
+    }
+
+    public static void badRequestException(String message, FieldInfo fieldInfo) {
+        throw new BadRequestException(message, fieldInfo);
+    }
+}
--- src/main/java/pl/kafarsoon/craftymarkdown/feature/file/loader/service/FileTransformerService.java
+++ src/main/java/pl/kafarsoon/craftymarkdown/feature/file/loader/service/FileTransformerService.java
@@ -4,10 +4,14 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
+import pl.kafarsoon.craftymarkdown.exception.handler.ErrorCode;
+import pl.kafarsoon.craftymarkdown.exception.handler.FieldInfo;
 import pl.kafarsoon.craftymarkdown.feature.file.loader.dto.FileDTO;
 
 import java.io.IOException;
 import java.util.Objects;
+
+import static pl.kafarsoon.craftymarkdown.exception.handler.ErrorsUtils.badRequestException;
 
 @Service
 @Log4j2
@@ -25,7 +29,9 @@ public class FileTransformerService {
             fileDTO.setExtension(splitFilename[1]);
         }
         if (!fileDTO.getExtension().equals("txt")) {
-            log.error("Not supported file extension. Required txt, but found :" + fileDTO.getExtension());
+            badRequestException("Not supported file extension. Required txt, but found :" + fileDTO.getExtension(), new FieldInfo("file extension", ErrorCode.BAD_REQUEST));
+//            notFoundException("Not found a product with this id: " + id + " .", new FieldInfo("id", ErrorCode.NOT_FOUND));
+
         }
         return fileDTO;
     }

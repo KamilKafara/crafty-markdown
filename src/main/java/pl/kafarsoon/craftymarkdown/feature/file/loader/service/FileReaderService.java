103e3e88ec0789b111143a7d80408a588909b8ea
Kamil Kafara <kafarakamil95@gmail.com>
2020-02-29 18:00:43 +0100

    CMD-1-field-loader: cleanup

--- src/main/java/pl/kafarsoon/craftymarkdown/feature/file/loader/service/FileReaderService.java
+++ src/main/java/pl/kafarsoon/craftymarkdown/feature/file/loader/service/FileReaderService.java
@@ -1,7 +1,6 @@
 package pl.kafarsoon.craftymarkdown.feature.file.loader.service;
 
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
@@ -9,7 +8,7 @@ import java.io.IOException;
 @Service
 public class FileReaderService {
 
-    public String fileReader(@RequestParam("file") MultipartFile file) throws IOException {
+    public String fileReader(MultipartFile file) throws IOException {
         if (!file.isEmpty()) {
             byte[] bytes = file.getBytes();
             String completeData = new String(bytes);

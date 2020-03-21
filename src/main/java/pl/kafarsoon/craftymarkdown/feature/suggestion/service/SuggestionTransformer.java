ede58dbb0047daa4799e1fa3d051abfe40f7131f
Kamil Kafara <kafarakamil95@gmail.com>
2020-03-21 12:53:52 +0100

    CMD-15-suggestion-entity: update transformer

--- src/main/java/pl/kafarsoon/craftymarkdown/feature/suggestion/service/SuggestionTransformer.java
+++ src/main/java/pl/kafarsoon/craftymarkdown/feature/suggestion/service/SuggestionTransformer.java
@@ -1,5 +1,6 @@
 package pl.kafarsoon.craftymarkdown.feature.suggestion.service;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 import pl.kafarsoon.craftymarkdown.feature.suggestion.dto.SuggestionDTO;
 import pl.kafarsoon.craftymarkdown.feature.suggestion.repository.Suggestion;
@@ -7,12 +8,12 @@ import pl.kafarsoon.craftymarkdown.feature.suggestion.repository.Suggestion;
 @Service
 public class SuggestionTransformer {
 
-    private SuggestionDTO convertToDTO(Suggestion suggestion) {
-        return null;
+    public SuggestionDTO convertToDTO(Suggestion suggestion) {
+        return new ModelMapper().map(suggestion, SuggestionDTO.class);
     }
 
-    private Suggestion convertFromDTO(SuggestionDTO suggestionDTO) {
-        return null;
+    public Suggestion convertFromDTO(SuggestionDTO suggestionDTO) {
+        return new ModelMapper().map(suggestionDTO, Suggestion.class);
     }
 
 }
